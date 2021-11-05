046764418ae6ea0fa526563514356a6029f9ad05
AritraCM <aritracm@gmail.com>
2021-11-05 02:10:15 -0400

    filenames

--- portfolio_website/settings.py
+++ portfolio_website/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['aritram.herokuapp.com',
+ALLOWED_HOSTS = ['aritraportfolio.herokuapp.com',
 '127.0.0.1' ]
 
 
